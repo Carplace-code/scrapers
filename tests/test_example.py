6d2db160d78e64db191a1178dae0d97acac7630b
Antonio Venegas <antonio.venegas@uc.cl>
2025-05-04 16:45:57 -0400

    feat: added fb crawler

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,8 +1,8 @@
-
-# test files must start with test_ or end with _test
+# test files must start with test_
 # test functions must start with test_
 def test_example():
-  assert True
+    assert True
+
 
 def test_example_2():
-  assert True
+    assert True
